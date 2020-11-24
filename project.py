55d66e7112a60301e4f40cc4cb5a2e80331717af
chayhyeon <bkindtoevery1@gist.ac.kr>
2020-11-25 03:14:52 +0900

    Complete using SQLite3 DB

--- project.py
+++ project.py
@@ -4,36 +4,30 @@ import sqlite3
 
 lock = threading.Lock()
 
-# Create Database Session
-DB_NAME = "dev_montoring.db"
+# Create Database
+DB_NAME = "dev_mentoring.db"
 
 app = Flask(__name__)
 #app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024 #16메가바이트
 app.secret_key = b'6rav3_C00k!e Is Str0n93r than Z0m6!3_c00k13'
 
-cookie = {1 : ['떼탈출 언제하지..', 'a', 0, '떼탈출 재밌긴 한데.. 살짝 귀찮음'], 2 : ['그챔 끝!', 'a', 0, '명랑한 쿠키양은 명랑했다.']}
-cookie_count = 2
-free = {}
-free_count = 0
-signed_id = None
-"""
-def check_session(id):
-    global signed_id
-    if signed_id != id:
-        return render_template('wrong_access.html')
-""" #이렇게 따로 함수를 불러서 하면 안되는걸까?
-
 def session_check(user_id): #얘는 user_id 받아서 html을 올리라고 해주니 이렇게 함수로 선언해도 괜찮다!
-    if not user_id in session['username']:
-        return render_template('wrong_access.html')
+    try:
+        if user_id in session['username']:
+            return True
+    except:
+         return render_template('wrong_access.html')
+
 
 @app.route('/')
 def main_page():
     return render_template('main_page.html')
 
+
 @app.route('/sign_up/')
 def sign_up():
     return render_template('sign_up.html')
+
 
 @app.route('/add_account', methods =['POST', "GET"])
 def add_account(ID=None, PW=None, PWcheck=None):
@@ -46,7 +40,6 @@ def add_account(ID=None, PW=None, PWcheck=None):
         if pw != pwcheck: #아이디 존재여부보다 이거먼저하는게 보안에 좀 더 나을듯
             return render_template('pwcheck_fail.html')
         # Account가 존재하는지?
-        conn = sqlite3.connect(DB_NAME)
         curr = conn.cursor()
 
         account = curr \
@@ -62,16 +55,18 @@ def add_account(ID=None, PW=None, PWcheck=None):
         return redirect(url_for('sign_in'))
     return render_template('wrong_access.html') #위에서 sign_up이 되든 안되든 딴 곳으로 감
 
+
 @app.route('/sign_in/')
 def sign_in():
     return render_template('login.html')
+
 
 @app.route('/auth', methods = ['POST', 'GET'])
 def auth(ID = None, PW = None):
     if request.method == 'GET':
         pass
     if request.method == "POST":
-
+        
         id = request.form['ID']
         pw = request.form['PW']
         # Get password from sqlite3 db
@@ -82,120 +77,167 @@ def auth(ID = None, PW = None):
             .fetchone()[0]
         conn.close()
         if pw == passwd:
-            session['username'] = request.form['ID']
-            return redirect(url_for('loggined_main_page', user_id = id))
+            session['username'] = id
+            return redirect(url_for('loggined_main_page', \
+                user_id = id))
+
     return render_template('login_fail.html')
+
 
 @app.route('/main/<user_id>/')
 def loggined_main_page(user_id):
-    # 앞으로 나오는 아래 세 줄은 세션 유지를 위한 검사
     session_check(user_id)
 
-    return render_template('loggined_main_page.html', user_id = user_id)
+    return render_template('loggined_main_page.html', \
+        user_id = user_id)
+
 
 @app.route('/logout')
 def logout():
     session.pop('username', None)
     return redirect(url_for('main_page'))
 
+
 @app.route('/main/<user_id>/<board_type>')
 def show_board(user_id, board_type):
     session_check(user_id)
 
-    global cookie, free
-    if board_type == 'cookie':
-        return render_template('show_board.html',user_id = user_id, board_type = board_type, num = len(cookie), data = dict(sorted(cookie.items(), reverse=True)))
-    elif board_type == 'free':
-        return render_template('show_board.html',user_id = user_id, board_type = board_type, num = len(free), data = dict(sorted(free.items(), reverse=True)))
+    conn = sqlite3.connect(DB_NAME)
+    curr = conn.cursor()
+    
+    if board_type == 'cookie' or board_type == 'free':
+        posts = curr \
+        .execute(f"SELECT * FROM {board_type}_board ") \
+        .fetchall()
+        conn.close()
+        print(posts)
+        return render_template('show_board.html', \
+            user_id = user_id, board_type = board_type, num = len(posts), data = posts)
+
     return render_template('wrong_access.html')
+
 
 @app.route('/main/<user_id>/<board_type>/<post_num>')
 def show_post(user_id, board_type, post_num):
     session_check(user_id)
 
-    global cookie, free
-    if board_type == 'cookie':
-        return render_template('show_post.html',user_id = user_id, board_type = board_type, post_num = post_num, data = cookie[int(post_num)])
-    else:
-        return render_template('show_post.html',user_id = user_id, board_type = board_type, post_num = post_num, data = free[int(post_num)])
+    conn = sqlite3.connect(DB_NAME)
+    curr = conn.cursor()
+
+    # try/except because the post which queried can be deleted by the author
+    try:
+        if board_type == 'cookie' or board_type == 'free':
+            post = curr \
+            .execute(f"SELECT * FROM {board_type}_board WHERE number = {post_num}") \
+            .fetchone()
+            conn.close()
+            return render_template('show_post.html', \
+                user_id = user_id, board_type = board_type, post_num = post_num, data = post)
+
+        else:
+            conn.close()
+            return render_template('wrong_access.html')
+
+    except:
+        conn.close()
+        return render_template('find_post_failure.html', \
+            user_id = user_id, board_type = board_type)
+
 
 @app.route('/main/<user_id>/<board_type>/write')
 def show_write(user_id, board_type):
     session_check(user_id)
     return render_template('write_post.html', user_id = user_id, board_type = board_type)
 
+
 @app.route('/main/<user_id>/<board_type>/add_post', methods = ['POST'])
 def add_post(user_id, board_type, Title=None, Content=None):
     session_check(user_id)
 
-    global cookie, free, cookie_count, free_count
-    post = [] #현재 쓴 글에 대한 정보
+    lock.acquire()
+
+    conn = sqlite3.connect(DB_NAME)
+    curr = conn.cursor()
+
     title = request.form['Title']
     content = request.form['Content']
-    post_num = -1
-    if board_type == 'cookie':
-        lock.acquire()
-        cookie_count += 1
-        time.sleep(5)
-        post_num = cookie_count
-        lock.release()
-    elif board_type == 'free':
-        lock.acquire()
-        free_count += 1
-        time.sleep(5)
-        post_num = free_count
-        lock.release()
-    else:
-        return render_template('wrong_access.html')
-    post.append(title)
-    post.append(user_id)
-    post.append(0) #처음 쓴 글의 좋아요 수는 0
-    post.append(content)
-    if board_type == 'cookie':
-        cookie[post_num] = post
-    else:
-        free[post_num] = post
-    return redirect(url_for('show_post', user_id = user_id, board_type = board_type, post_num = post_num))
+    like = 0
+    post_num = 0
+
+    if board_type == 'cookie' or board_type == 'free':
+        curr.execute(f"INSERT INTO {board_type}_board (title, author, like, content) VALUES \
+            ('{title}', '{user_id}', {like}, '{content}')")
+        conn.commit()
+
+        # 방금 INSERT한 친구의 number를 갖고오는 SQLite 내장 함수!
+        post_num = curr.execute("SELECT LAST_INSERT_ROWID()").fetchone()[0]
+    
+    conn.close()
+
+    lock.release()
+    return redirect(url_for('show_post', \
+        user_id = user_id, board_type = board_type, post_num = post_num))
+
 
 @app.route('/main/<user_id>/<board_type>/rewrite/<post_num>')
 def show_rewrite(user_id, board_type, post_num):
     session_check(user_id)
 
+    conn = sqlite3.connect(DB_NAME)
+    curr = conn.cursor()
+
+    post = curr \
+        .execute(f"SELECT * FROM {board_type}_board WHERE number = {post_num}") \
+        .fetchone()
+    conn.close()
+
     if board_type == 'cookie':
-        return render_template('rewrite_post.html', user_id = user_id, board_type = board_type, post_num = post_num, data = cookie[int(post_num)-1])
+        return render_template('rewrite_post.html', \
+            user_id = user_id, board_type = board_type, post_num = post_num, data = post)
     elif board_type == 'free':
-        return render_template('rewrite_post.html', user_id = user_id, board_type = board_type, post_num = post_num, data = free[int(post_num)-1])
+        return render_template('rewrite_post.html', \
+            user_id = user_id, board_type = board_type, post_num = post_num, data = post)
+    
     return render_template('wrong_access.html')
 
-@app.route('/main/<user_id>/<board_type>/fix_post/<post_num>/<post_index>', methods = ['POST'])
-def fix_post(user_id, board_type, post_num, post_index, Title = None, Content = None):
+
+@app.route('/main/<user_id>/<board_type>/fix_post/<post_num>', methods = ['POST'])
+def fix_post(user_id, board_type, post_num, Title = None, Content = None):
     session_check(user_id)
+
+    conn = sqlite3.connect(DB_NAME)
+    curr = conn.cursor()
 
     title = request.form['Title']
     content = request.form['Content']
-    index = int(post_index)
-    if board_type != 'cookie' and  board_type != 'free':
+    if board_type == 'cookie' or board_type == 'free':
+        curr.execute(f"UPDATE {board_type}_board \
+            SET title = '{title}', content = '{content}' WHERE number = {post_num}")
+
+        conn.commit()
+        conn.close()
+        return redirect(url_for('show_post', user_id = user_id, board_type = board_type, post_num = post_num))
+    
+    else:
         return render_template('wrong_access.html')
 
-    elif board_type == 'cookie':
-        cookie[index][0] = title
-        cookie[index][3] = content
-    else:
-        free[index][0] = title
-        free[index][3] = content
-    return redirect(url_for('show_post', user_id = user_id, board_type = board_type, post_num = post_num))
 
 @app.route('/main/<user_id>/<board_type>/delete/<post_num>', methods = ['POST'])
 def delete(user_id, board_type, post_num, Data = None):
     session_check(user_id)
-    global cookie, free
-    if board_type == 'cookie':
-        cookie.pop(int(post_num))
+    
+    if board_type == 'cookie' or  board_type == 'free':
+        conn = sqlite3.connect(DB_NAME)
+        curr = conn.cursor()
+
+        curr.execute(f"DELETE FROM {board_type}_board WHERE number = {post_num}")
+
+        conn.commit()
+        conn.close()
         return redirect(url_for('show_board', user_id = user_id, board_type = board_type))
-    elif board_type == 'free':
-        free.pop(int(post_num))
-        return redirect(url_for('show_board', user_id = user_id, board_type = board_type))
-    return render_template('wrong_access.html')
+    else:
+        return render_template('wrong_access.html')
+
 
 if __name__ == '__main__':
     app.run(debug = True)
--- tables.py
+++ tables.py
@@ -1,7 +1,7 @@
 import sqlite3
 
 
-DB_NAME = "dev_montoring.db"
+DB_NAME = "dev_mentoring.db"
 
 
 if __name__ == "__main__":
@@ -10,10 +10,47 @@ if __name__ == "__main__":
 
     c = conn.cursor()
     # Create account table
-    c.execute('''CREATE TABLE IF NOT EXISTS account (id text, password text)''')
+    c.execute('''CREATE TABLE IF NOT EXISTS account (
+        id text PRIMARY KEY, 
+        password text
+    )''')
+    # 처음에 따옴표 세 개로 하길래 이게 뭐지.. 파이썬에서는 따옴표 세 개면 주석인데 싶었는데..
+    # 알고 보니 여러 줄 문자열이었던 것이다!
 
     # 가계정 insert
-    c.execute('''INSERT INTO account VALUES ('admin', 'brave'), ('a', 'a')''')
+    c.execute('''INSERT INTO account VALUES 
+        ('admin', 'brave'), 
+        ('a', 'a'),
+        ('NULL', 'd336132dbbd2be791aed44dfdf1ac806efa8ddcab05faff75b6f412ef387cbd944983da12cce87fe0c485ce0fe35790d44e4a87bb532efdd901a19464f653831')
+    ''')
+    # NULL의 비밀번호는 NULL...을 SHA-512로 암호화한 것!
+
+    #Create Boards
+
+    #Create free board table
+    c.execute('''CREATE TABLE IF NOT EXISTS free_board (
+        number integer PRIMARY KEY AUTOINCREMENT, 
+        title text, 
+        author text, 
+        like integer, 
+        content text
+    )''' )
+    
+    #Create free board table
+    c.execute('''CREATE TABLE IF NOT EXISTS cookie_board (
+        number integer PRIMARY KEY AUTOINCREMENT, 
+        title text, 
+        author text, 
+        like integer, 
+        content text
+    )''' )
+
+    c.execute('''INSERT INTO cookie_board (title, author, like, content) VALUES
+        ('떼탈출 언제하지..', 'a', 0, '떼탈출 재밌긴 한데.. 살짝 귀찮음'),
+        ('그챔 끝!', 'a', 0, '명랑한 쿠키양은 명랑했다.')
+    ''')
+
+    # create meta-datas like global variables
 
     conn.commit()
 
